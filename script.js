245a1c23f462c0a6c32c8ae5481a6e88ba9d5be9
Davi Ramos Bonilho <00001119459850sp@aluno.educacao.sp.gov.br>
2026-09-16 08:44:30 -0300

    fix: corrigir exibicao do resultado e tag script

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ function somar() {
   let numero1 = Number(document.getElementById("numero1").value);
   let numero2 = Number(document.getElementById("numero2").value);
   let resultado = numero1 + numero2;
-  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  document.getElementById("valor-resultado").innerHTML = resultado;
 }
 
 function subtrair() {
   let numero1 = Number(document.getElementById("numero1").value);
   let numero2 = Number(document.getElementById("numero2").value);
   let resultado = numero1 - numero2;
-  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  document.getElementById("valor-resultado").innerHTML = resultado;
 }
 
 function multiplicar() {
   let numero1 = Number(document.getElementById("numero1").value);
   let numero2 = Number(document.getElementById("numero2").value);
   let resultado = numero1 * numero2;
-  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  document.getElementById("valor-resultado").innerHTML = resultado;
 }
 
 function dividir() {
@@ -24,9 +24,10 @@ function dividir() {
   let numero2 = Number(document.getElementById("numero2").value);
 
   if (numero2 === 0) {
-    document.getElementById("resultado").innerHTML = "Erro: Divisão por zero!";
+    document.getElementById("valor-resultado").innerHTML =
+      "Erro: Divisão por zero!";
   } else {
     let resultado = numero1 / numero2;
-    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+    document.getElementById("valor-resultado").innerHTML = resultado;
   }
 }
