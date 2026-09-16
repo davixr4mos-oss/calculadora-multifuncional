f1232f9d9399dbdfddb49f8527ce99054103ef49
Davi Ramos Bonilho <00001119459850sp@aluno.educacao.sp.gov.br>
2026-09-16 08:35:45 -0300

    feat: adicionar multiplicacao, divisao e workflow de automacao

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 function somar() {
-    let numero1 = Number(document.getElementById("numero1").value);
-    let numero2 = Number(document.getElementById("numero2").value);
-
-    let resultado = numero1 + numero2;
-
-    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+  let resultado = numero1 + numero2;
+  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
 }
 
 function subtrair() {
-    let numero1 = Number(document.getElementById("numero1").value);
-    let numero2 = Number(document.getElementById("numero2").value);
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+  let resultado = numero1 - numero2;
+  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}
 
-    let resultado = numero1 - numero2;
+function multiplicar() {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+  let resultado = numero1 * numero2;
+  document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}
 
+function dividir() {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+
+  if (numero2 === 0) {
+    document.getElementById("resultado").innerHTML = "Erro: Divisão por zero!";
+  } else {
+    let resultado = numero1 / numero2;
     document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
 }
